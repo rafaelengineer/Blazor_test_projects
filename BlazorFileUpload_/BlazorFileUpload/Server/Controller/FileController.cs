5921f3bb9eb26dae3e55de2324fed71d7dea3d26
rafael_so <rafael.soares@tseaenergia.com.br>
2024-02-01 10:09:43 -0300

    01.fev

---
+++ BlazorFileUpload_/BlazorFileUpload/Server/Controller/FileController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BlazorFileUpload.Shared;
+using System.Net;
+
+
+namespace BlazorFileUpload.Server.Controller
+{
+    //[Route("api/[controller]")]
+    [Route("api/File")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public FileUploadController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<UploadResult>>> Uploadfile(List<IFormFile> files)
+        {
+            List<UploadResult> uploadResults = new List<UploadResult>();
+            foreach (var file in files)
+            {
+                var uploadResult = new UploadResult();
+                string trustedFileNameForFileStorage;
+                var untrustedFileName = file.FileName;
+                uploadResult.FileName = untrustedFileName;
+                var trustedFileNameForDisplay = WebUtility.HtmlEncode(untrustedFileName);
+
+                //trustedFileNameForFileStorage = Path.GetRandomFileName();//original on tutorial.
+                trustedFileNameForFileStorage = file.FileName;//
+                var path = Path.Combine(_env.ContentRootPath, "uploads", trustedFileNameForFileStorage);
+
+                await using FileStream fs = new(path, FileMode.Create);
+                await file.CopyToAsync(fs);
+
+                uploadResult.StoredFileName = trustedFileNameForDisplay;
+                uploadResults.Add(uploadResult);
+            }
+
+            return Ok(uploadResults);
+        }
+    }
+}
---
+++ BlazorFileUpload_/BlazorFileUpload/Server/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Net.Http.Headers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseCors(policy =>
+    policy.WithOrigins("http://localhost:5290", "http://localhost:5290")
+    .AllowAnyMethod()
+    .WithHeaders(HeaderNames.ContentType)
+);
+
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
